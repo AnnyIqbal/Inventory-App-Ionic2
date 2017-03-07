ef352960794db849661a12421e4acd9cc9e40239
Anny <quratulann_iqbal@hotmail.com>
2017-03-07 13:54:20 +0500

    deploying

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,20 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { AngularFireModule } from 'angularfire2';
 import { MyApp } from './app.component';
 import { StorePage } from '../pages/store/store';
 import { WishlistPage } from '../pages/wishlist/wishlist';
 import { TabsPage } from '../pages/tabs/tabs';
 import { ProductRowComponent } from '../pages/product-row/product-row.component';
 import { ProductsListComponent } from '../pages/products-list/products-list.component';
+
+const config = {
+    apiKey: "AIzaSyAONYjHE2LHnXYB2m0utQIORUkURrle1zU",
+    authDomain: "ionic-inventoryapp.firebaseapp.com",
+    databaseURL: "https://ionic-inventoryapp.firebaseio.com",
+    storageBucket: "ionic-inventoryapp.appspot.com",
+    messagingSenderId: "654746523864"
+  };
 
 @NgModule({
   declarations: [
@@ -17,7 +26,8 @@ import { ProductsListComponent } from '../pages/products-list/products-list.comp
     ProductsListComponent
   ],
   imports: [
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+        AngularFireModule.initializeApp(config)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
